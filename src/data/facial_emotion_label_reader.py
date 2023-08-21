8672e5b3076e33628a8c0160ddaa6b57ca9c2b84
Nektarios Totikos <Totikos@web.de>
2023-08-21 22:35:16 +0200

    Add plant data reader and adjust .gitignore

---
+++ src/data/facial_emotion_label_reader.py
@@ -0,0 +1,3 @@
+"""Label extractor for plant data. Labels are received from facial emotions."""
+
+
---
+++ src/data/plant_data_reader.py
@@ -0,0 +1,15 @@
+"""Data reader for plant data stored as .wav files."""
+
+from src.utils.constants import PLANT_DIR, PROJECT_ROOT, path
+print(f"Let's try this first.: {PLANT_DIR}")
+
+from src.utils.constants import PLANT_DIR
+
+print(f"Let's try this hehe: {PLANT_DIR}")
+
+print(f"Project root: {PROJECT_ROOT}")
+
+print(f"cwd: {path}")
+
+
+
