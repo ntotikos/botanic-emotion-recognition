d78dc6f6dc5bc8472dba441201132ce102193a12
Nektarios Totikos <Totikos@web.de>
2023-08-26 03:27:51 +0200

    Added imports from constants.py

--- src/data/data_factory.py
+++ src/data/data_factory.py
@@ -1,7 +1,33 @@
-"""Create datasets."""
+"""Create datasets.Load labels and features/1s time series from disk and create the actual dataset. """
 
 import os
+from src.utils.constants import TEAM_NAMES, TEAMWORK_SESSION_DAYS
 
 
 def map_emotions_to_signal(matches) -> None:
+    """
+    Load labels and features/1s time series from disk and create the actual dataset.
+    """
     print("Hi.")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    for team in TEAM_NAMES:
+        for day in TEAMWORK_SESSION_DAYS:
+            pass
