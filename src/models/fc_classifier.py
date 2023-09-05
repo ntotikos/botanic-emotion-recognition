d8c9a3d2d77e6c7b95ea04efea792fcf92918e74
Nektarios Totikos <Totikos@web.de>
2023-09-05 02:38:08 +0200

    Run training on roughly 80000 samples

--- src/models/fc_classifier.py
+++ src/models/fc_classifier.py
@@ -2,6 +2,7 @@
 from src.data.dataset_builder import EkmanDataset
 from src.models.deeplearning_classifier import DLClassifier
 import torch
+from tqdm import tqdm
 
 from src.utils.constants import DATASETS_DIR
 
@@ -72,7 +73,7 @@ def _main_pseudo_training():
             print(f"Epoch [{epoch + 1}/{epochs}], Loss: {loss.item():.4f}")
 
 
-def _main():
+def _main_one_file():
     path_to_pickle = DATASETS_DIR / "sdm_2023-01-10_team_01_8333_9490.pkl"
     dataset = EkmanDataset(path_to_pickle)
     dataset.get_data_and_labels()
@@ -98,6 +99,55 @@ def _main():
 
     # Number of epochs
     epochs = 200
+
+    # Training loop
+    for epoch in range(epochs):
+        for batch_data, batch_labels in train_dataloader:
+            # Zero gradients
+            model.optimizer.zero_grad()
+
+            # Forward pass
+            outputs = model(batch_data)  # Without implemented __call__ method: model.forward(data)
+
+            # Compute loss
+            loss = model.criterion(outputs, batch_labels)
+
+            # Backward pass and optimize
+            loss.backward()
+            model.optimizer.step()
+
+        if (epoch + 1) % 10 == 0:
+            print(f"Epoch [{epoch + 1}/{epochs}], Loss: {loss.item():.4f}")
+
+
+def _main():
+    path_to_pickle = DATASETS_DIR / "sdm_2023-01_all_valid_files_version_1.pkl"
+    dataset = EkmanDataset(path_to_pickle)
+    dataset.get_data_and_labels()
+    dataset.split_dataset_into_train_val_test()
+
+    train_dataloader, val_dataloader, test_dataloader = dataset.create_data_loader()
+
+    dataset.get_label_distribution(train_dataloader)
+
+    # TODO: use correct input and output dimensions
+    input_dim = 10000
+    output_dim = 7
+    # Model
+    model = DenseClassifier("params")
+    model.setup_model()
+    model.model = torch.nn.Sequential(
+        torch.nn.Linear(input_dim, 128),
+        torch.nn.ReLU(),
+        torch.nn.Linear(128, 64),
+        torch.nn.ReLU(),
+        torch.nn.Linear(64, output_dim)
+        # torch.nn.Softmax(dim=1) -> not needed because torch.nn.CrossEntropyLoss inherently applies softmax
+        )
+    model.setup_training()
+
+    # Number of epochs
+    epochs = 50
 
     # Training loop
     for epoch in range(epochs):
