c8fd6bbdb9e683d8e684b5974925bb1cb15b2e40
Nektarios Totikos <Totikos@web.de>
2023-09-05 02:42:50 +0200

    Implement pickle file generation for ca 80000 samples

--- src/data/data_segmentation.py
+++ src/data/data_segmentation.py
@@ -1,13 +1,21 @@
 """Slice .wav plant data into 1s chunks and assign cleaned labels. Store as pickle."""
+import glob
 import os
-from typing import List
+from typing import List, Dict
 
 import pandas as pd
 from scipy.io import wavfile
 import numpy as np
 import pickle
+import logging
 
-from src.utils.constants import CLEANED_DATA_DIR, LABELS_DIR, DATASETS_DIR
+from src.utils.constants import CLEANED_DATA_DIR, LABELS_DIR, DATASETS_DIR, TEAM_NAMES_CLEANED, LOGS_DIR
+
+logging.basicConfig(filename=LOGS_DIR / 'data-cleaning/data_segmentation_all_mismatches_DEBUG.log',
+                    filemode='w',
+                    level=logging.DEBUG,
+                    format='%(asctime)s - %(levelname)s %(message)s',
+                    datefmt='%m/%d/%Y %I:%M:%S %p')
 
 
 def read_plant_file(filepath: str):
@@ -57,12 +65,12 @@ def map_slice_to_emotion(slices, dataframe_emotions):
     One segment consists of a 1s signal portion and the corresponding label (including IDs).
     """
     segments = []
-    for i in range(len(slices)):
+    for segmend_id in range(len(slices)):
         segment = {
-            "wav_slice": slices[i],
-            "label": dataframe_emotions["Labels"].iloc[i],
-            "segment_id": i,
-            "frame_id": dataframe_emotions["Frame"].iloc[i]
+            "wav_slice": slices[segmend_id],
+            "label": dataframe_emotions["Labels"].iloc[segmend_id],
+            "segment_id": segmend_id,
+            "frame_id": dataframe_emotions["Frame"].iloc[segmend_id]
         }
         segments.append(segment)
     return segments
@@ -71,13 +79,70 @@ def map_slice_to_emotion(slices, dataframe_emotions):
 def save_datapoints(datapoints):
     # TODO: it might be worthwhile to store the files into database in the future in case of frequent query.
 
-    save_file_path = DATASETS_DIR / 'sdm_2023-01-10_team_01_8333_9490.pkl'
+    # save_file_path = DATASETS_DIR / 'sdm_2023-01-10_team_01_8333_9490.pkl'
+    save_file_path = DATASETS_DIR / 'sdm_2023-01_all_valid_files_version_1.pkl'
+
     if not os.path.isfile(save_file_path):
         with open(save_file_path, 'wb') as file:
             pickle.dump(datapoints, file)
+    else:
+        print(f"File already exists: {save_file_path}.")
 
 
-if __name__ == "__main__":
+def get_all_filenames_in_dir(root_path) -> Dict:
+    # TODO: Maybe I should use os.path.walk and traverse the tree?
+    all_filenames_dir = {}
+
+    # Loop through each main class folder
+    for class_folder in os.listdir(root_path):
+        class_path = os.path.join(root_path, class_folder)
+
+        # Ensure the path is a directory
+        if os.path.isdir(class_path):
+            all_filenames_dir[class_folder] = []
+
+            # Collect paths from each subfolder
+            for subfolder in os.listdir(class_path):
+                subfolder_path = os.path.join(class_path, subfolder)
+
+                # Check if it's indeed a subfolder
+                if os.path.isdir(subfolder_path):
+                    image_files = glob.glob(os.path.join(subfolder_path, '*.*'))
+                    all_filenames_dir[class_folder].extend(image_files)
+
+    return all_filenames_dir
+
+
+def check_durations(data_filename: str, emotion_filename: str) -> bool:
+    """
+    Checks the durations derived from the start and end times incorporated in the filenames.
+    :param data_filename: file name of the signal file.
+    :param emotion_filename: file name of the cleaned emotion file.
+    """
+    same_durations = True
+    interval_emotion_file = emotion_filename.split(".")[0].split("_")[-4:]
+    interval_data_file = data_filename.split(".")[0].split("_")[-4:]
+
+    if interval_emotion_file != interval_data_file:
+        same_durations = False
+        print("Labels don't match.")
+    #else:
+        #print("Labels are the same.")
+
+    return same_durations
+
+
+def flatten_list_of_lists(list_of_lists):
+    print(f"shape list_of_lists: {len(list_of_lists)}, {len(list_of_lists[0])}, {len(list_of_lists[1])}")
+    flattened_list = []
+    for sublist in list_of_lists:
+        flattened_list.extend(sublist)
+    print(f"shape flattened_list: {len(flattened_list)}")
+
+    return  flattened_list
+
+
+def _main_one_file():
     wav_file_path_test = CLEANED_DATA_DIR / r"team_01\2023-01-10\sdm_2023-01-10_team_01_8333_9490.wav"
     emotions_file_path_test = LABELS_DIR / r"team_01\2023-01-10\emotions_sdm_2023-01-10_team_01_8333_9490.csv"
 
@@ -88,3 +153,53 @@ if __name__ == "__main__":
     # Data points for one file! TODO: use for loop to iterate over all files!
     data_points = map_slice_to_emotion(signal_slices, emotions)
     save_datapoints(data_points)
+
+
+def _main():
+    #wav_file_path_test = CLEANED_DATA_DIR
+    #emotions_file_path_test = LABELS_DIR
+
+    datafiles_dir = get_all_filenames_in_dir(CLEANED_DATA_DIR)
+    labelfiles_dir = get_all_filenames_in_dir(LABELS_DIR)
+    count = 0
+    data_points_all = []
+    for team in TEAM_NAMES_CLEANED:
+        for idx in range(len(labelfiles_dir[team])):
+            samplingrate, signal = read_plant_file(datafiles_dir[team][idx])
+            signal_slices = slice_plant_signal(signal, samplingrate)
+            emotions = read_cleaned_emotions(labelfiles_dir[team][idx])
+
+            #print(check_durations(datafiles_dir[team][idx], labelfiles_dir[team][idx]))
+
+            interval_emotion_file = labelfiles_dir[team][idx].split(".")[0].split("_")[-4:]
+            #print("duration from label: ", int(interval_emotion_file[-1])-int(interval_emotion_file[-2]))
+
+            if int(interval_emotion_file[-1])-int(interval_emotion_file[-2]) == len(emotions):
+                #    print(check_durations(datafiles_dir[team][idx], labelfiles_dir[team][idx]))
+                #if len(signal_slices) != len(emotions):
+                logging.info("-----")
+                logging.info(f"team, idx: {team}, {idx}")
+                logging.info(f"data file: {datafiles_dir[team][idx]}")
+                logging.info(f"label file: {labelfiles_dir[team][idx]}")
+                count += 1
+                logging.info(f"length slices, emotions: {len(signal_slices)}, {len(emotions)}")
+
+                data_points = map_slice_to_emotion(signal_slices, emotions)
+                data_points_all.append(data_points)
+            #else:
+            #    count += 1
+            #    print(f"Information about the teamwork session duration do not match. ")
+    # SAVE DATA
+    logging.info(f"count:{count}")
+
+    data_points_all = flatten_list_of_lists(data_points_all)
+    print(len(data_points_all))
+
+
+    # Data points for one file! TODO: use for loop to iterate over all files!
+    #(data_points_all)
+
+
+if __name__ == "__main__":
+    _main()
+
