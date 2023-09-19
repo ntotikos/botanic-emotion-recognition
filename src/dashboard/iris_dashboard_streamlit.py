5ba0176ee53922351aefb88848b12602225353b5
Nektarios Totikos <Totikos@web.de>
2023-09-19 23:43:34 +0200

    Add some components to dashboard

--- src/dashboard/iris_dashboard_streamlit.py
+++ src/dashboard/iris_dashboard_streamlit.py
@@ -6,6 +6,25 @@ import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
 from sklearn.datasets import load_iris
+import time
+#plotly.express as px
+
+# Streamlit page setup
+st.set_page_config(
+    page_title="Real-Time Data Science Dashboard",
+    page_icon="✅",
+    layout="wide",
+)
+
+# Get data.
+dataset_url = "https://raw.githubusercontent.com/Lexie88rus/bank-marketing-analysis/master/bank.csv"
+
+# read csv from a URL
+@st.cache_data
+def get_data() -> pd.DataFrame:
+    return pd.read_csv(dataset_url)
+
+df = get_data()
 
 # Load the dataset
 iris = load_iris()
@@ -19,6 +38,8 @@ st.write("""
 This is a simple Streamlit dashboard that allows you to explore the Iris dataset.
 Select the visualization types from the sidebar.
 """)
+
+sepal_length = st.selectbox("Select the Job", pd.unique(df['sepal length (cm)']))
 
 x = np.linspace(0, 10, 100)
 y = np.sin(x)
