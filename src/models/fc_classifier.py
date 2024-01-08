90e1e316ebbfcba7ff522c4a6889240dfe7e30e3
Nektarios Totikos <Totikos@web.de>
2024-01-08 14:35:54 +0100

    Add notebook for cwt fcnn

--- src/models/fc_classifier.py
+++ src/models/fc_classifier.py
@@ -685,8 +685,8 @@ def main_test():
 
 if __name__ == "__main__":
     #_main(False)
-    main_hp_optimization()  # raw TS
-    #main_hp_optimization_spectral()  # MFCCs + DWT (level 1 & 3) + CWT
+    #main_hp_optimization()  # raw TS
+    main_hp_optimization_spectral()  # MFCCs + DWT (level 1 & 3) + CWT
 
 
     # Latest code for also reproducing my results:
